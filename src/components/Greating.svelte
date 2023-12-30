8f2a50398f4ae143ac29e93ff6b930ef56ce11b5
Yakov Kanter <ykanter@gmail.com>
2023-12-30 11:38:34 -0500

    A simple svelte component example

---
+++ src/components/Greating.svelte
@@ -0,0 +1,16 @@
+<script>
+  let name = "";
+</script>
+
+<label class="ml-10" for="myname">Your name: </label>
+<input
+  class="bg-blue-100 rounded-md"
+  name="myname"
+  type="text"
+  bind:value={name}
+/>
+{#if name.length > 0}
+  <div class="ml-10">
+    Hello {name}
+  </div>
+{/if}
