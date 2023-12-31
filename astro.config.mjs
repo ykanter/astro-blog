9a248d33b31c6bad06921f96b44f7f395842150f
Yakov Kanter <ykanter@gmail.com>
2023-12-31 09:36:20 -0500

    add netlify adapter for server side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  output : "server"
+  output: "server",
+  adapter: netlify()
 });
