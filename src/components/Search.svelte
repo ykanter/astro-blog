fa031ba78f821ca736f3dc30fc4d6511cd85f329
Yakov Kanter <ykanter@gmail.com>
2023-12-31 09:25:47 -0500

    small visual improvement to the search component

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -2,7 +2,11 @@
   let searchItem = "";
 </script>
 
-<a class="ml-10" href={`/search/${searchItem}`}>Search for: </a>
+<a class="ml-10" href={`/search/${searchItem}`}
+  ><span class="font-bold bg-blue-900 text-white rounded-full px-3 py-1"
+    >Search</span
+  > for:
+</a>
 <input
   class="bg-blue-100 rounded-md"
   type="text"
